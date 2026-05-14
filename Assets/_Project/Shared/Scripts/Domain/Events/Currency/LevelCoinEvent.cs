8c0fa028e64ceb2a0040dfc91d6095b7fa11ad98
DongChiMin <namv2704@gmail.com>
2026-05-14 13:51:05 +0700

    Xóa các file thừa ko còn sử dụng

--- Assets/_Project/Shared/Scripts/Domain/Events/Currency/LevelCoinEvent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace LabDiner.Shared.Event
-{
-    // [CreateAssetMenu(fileName = "OnCoinAdded", menuName = "Events/Currency/Coin Event")]
-    // public class LevelCoinEvent : GameEvent<double> { }
-}
--- Assets/_Project/Shared/Scripts/Domain/Events/Currency/LevelGemEvent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace LabDiner.Shared.Event
-{
-    // [CreateAssetMenu(fileName = "OnGemAdded", menuName = "Events/Currency/Gem Event")]
-    // public class LevelGemEvent : GameEvent<int> { }
-}
