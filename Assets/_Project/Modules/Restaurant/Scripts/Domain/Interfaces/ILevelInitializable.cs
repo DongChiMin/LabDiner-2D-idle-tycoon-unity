07c0f27fd31c2ffc6ff578d14336d05459674c1a
DongChiMin <namv2704@gmail.com>
2026-07-22 16:42:53 +0700

    thêm note cho 2 interface

--- Assets/_Project/Modules/Restaurant/Scripts/Domain/Interfaces/ILevelInitializable.cs
+++ Assets/_Project/Modules/Restaurant/Scripts/Domain/Interfaces/ILevelInitializable.cs
@@ -2,6 +2,9 @@ using LabDiner.Restaurant.SO;
 
 namespace LabDiner.Restaurant.Interface
 {
+    /// <summary>
+    /// Interface cho các class được gọi ở phase 2 trong LevelLoader, khởi tạo các dữ liệu và thành phần prefab
+    /// </summary>
     public interface ILevelInitializable
     {
         public void Init(LevelConfigSO config);
--- Assets/_Project/Modules/Restaurant/Scripts/Domain/Interfaces/ILevelProgress.cs
+++ Assets/_Project/Modules/Restaurant/Scripts/Domain/Interfaces/ILevelProgress.cs
@@ -3,6 +3,9 @@ using LabDiner.Shared;
 
 namespace LabDiner.Restaurant.Interface
 {
+    /// <summary>
+    /// Interface cho các class được gọi ở phase 3 trong LevelLoader, load dữ liệu từ save game
+    /// </summary>
     public interface ILevelProgress
     {
         public void LoadProgress();
